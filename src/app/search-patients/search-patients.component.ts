40f79c618ad4591445034141f2141c55a2fb8afe
ankur garha <ankur.garha@hcl.com>
2020-07-12 12:58:59 +0200

    add new patient serach module

---
+++ src/app/search-patients/search-patients.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-patients',
+  templateUrl: './search-patients.component.html',
+  styleUrls: ['./search-patients.component.scss']
+})
+export class SearchPatientsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
