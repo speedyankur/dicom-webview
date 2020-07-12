31eb9cbd4fef18e85077f5b1bbf1a63e60d70c7a
ankur garha <ankur.garha@hcl.com>
2020-07-12 12:58:45 +0200

    added bootstrap and added header tool bar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,18 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { SearchPatientsComponent } from './search-patients/search-patients.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchPatientsComponent
   ],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule, // new modules added here
+    MatToolbarModule,
     AppRoutingModule,
     BrowserAnimationsModule
   ],
