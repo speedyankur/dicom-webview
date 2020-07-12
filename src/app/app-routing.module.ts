64f0c4f312d565f05714de0b048ca899ed94b0f9
ankur garha <ankur.garha@hcl.com>
2020-07-12 13:05:10 +0200

    Added route for patient searching

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {SearchPatientsComponent} from './search-patients/search-patients.component'
 
-
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'patientSearch', component: SearchPatientsComponent },
+  { path: '', redirectTo: '/patientSearch', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
